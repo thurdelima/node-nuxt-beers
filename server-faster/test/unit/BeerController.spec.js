faca39c8d815804e07f5b01a99a0369502ed21b4
Arthur Lima <thurdelima@gmail.com>
2023-12-07 03:32:35 -0300

    test: post request BeerController and some adjusts

--- server-faster/test/unit/BeerController.spec.js
+++ server-faster/test/unit/BeerController.spec.js
@@ -1,17 +1,22 @@
 // test/unit/BeerController.spec.js
-const BeersController = use('App/Controllers/Http/BeerController');
+const BeerController = use('App/Controllers/Http/BeerController');
 const sinon = require('sinon');
+const { assert } = require('chai');
 const Beer = use('App/Models/Beer');
 
-const { test, trait } = use('Test/Suite')('BeerController test suite');
+const { test, trait, beforeEach } = use('Test/Suite')('BeerController test suite');
 trait('Test/ApiClient');
+
+// beforeEach(() => {
+//   sinon.stub(Beer, 'create');
+// });
 
 test('should return a list of beers', async ({ assert }) => {
   const fakeBeers = [{ name: 'Beer 1' }, { name: 'Beer 2' }];
   const beerAllStub = sinon.stub(Beer, 'all').resolves(fakeBeers);
   const responseMock = { json: sinon.stub() };
 
-  const beersController = new BeersController();
+  const beersController = new BeerController();
   await beersController.getAllBeers({ response: responseMock });
 
   assert.isTrue(beerAllStub.calledOnce);
@@ -25,7 +30,7 @@ test('should handle internal server error', async ({ assert }) => {
   const beerAllStub = sinon.stub(Beer, 'all').rejects(new Error(errorMessage));
   const responseMock = { status: sinon.stub().returnsThis(), json: sinon.stub() };
 
-  const beersController = new BeersController();
+  const beersController = new BeerController();
   await beersController.getAllBeers({ response: responseMock });
 
   assert.isTrue(beerAllStub.calledOnce);
@@ -33,5 +38,55 @@ test('should handle internal server error', async ({ assert }) => {
   assert.isTrue(responseMock.json.calledWith({ error: 'Internal Server Error' }));
 
   beerAllStub.restore();
+});
+
+
+
+test('should create a new beer', async () => {
+  
+  const requestMock = {
+    only: sinon.stub().returns({
+      name: 'Test Beer',
+      description: 'Test description',
+      image: 'test.jpg',
+      category_id: 1,
+    }),
+  };
+  
+  const responseMock = {
+    status: sinon.stub().returnsThis(),
+    json: sinon.stub(),
+  };
+
+  const fakeBeerData = {
+    id: 1,
+    name: 'Test Beer',
+    description: 'Test description',
+    image: 'test.jpg',
+    category_id: 1,
+  };
+
+  sinon.stub(Beer, 'create').resolves(fakeBeerData);
+
+  
+  const beersController = new BeerController();
+  await beersController.create({ request: requestMock, response: responseMock });
+
+  
+  assert.isTrue(Beer.create.calledOnce);
+  
+  assert.isTrue(responseMock.status.calledWith(201));
+  assert.isTrue(responseMock.json.calledOnce);
+  
+  assert.deepEqual(responseMock.json.firstCall.args[0], {
+    id: 1,
+    name: 'Test Beer',
+    description: 'Test description',
+    image: 'test.jpg',
+    category_id: 1,
+  });
+
+  
+  sinon.restore();
 });
 
