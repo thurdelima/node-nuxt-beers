f9d3523b6f8c6676f75d79c3e0d724bdd5cc0e82
Arthur Lima <thurdelima@gmail.com>
2023-12-08 06:11:30 -0300

    feat: integrate page category details with backend

--- front-faster/pages/category/[id].vue
+++ front-faster/pages/category/[id].vue
@@ -1,143 +1,117 @@
 <template>
   <div class="container-fluid">
     <div class="dashboard-title-cs">
-      <h2 class="display-5">
-        Category {{ $route.params.id }} <span class="emoji">🔎</span>
+      <h2 v-if="loading" class="display-5">
+        <p class="card-text placeholder-glow">
+          <span class="placeholder col-3 h-loading"></span>
+        </p>
       </h2>
-      
+      <h2 v-else class="display-5">
+        Category {{ categoriesData.category.name }}
+        <span class="emoji">🔎</span>
+      </h2>
     </div>
   </div>
-
- 
 
   <div class="container-fluid">
     <div class="dashboard-title-cs">
-      <h3 class="display-6">
-        Beers of category <span class="emoji">🍻</span>
-      </h3>
-      
+      <h3 class="display-6">Beers of category <span class="emoji">🍻</span></h3>
     </div>
   </div>
 
-  <div class="table-scrollable">
+  <div v-if="loading" class="text-center">
+    <p class="card-text placeholder-glow">
+      <span class="placeholder col-12 h-loading"></span>
+      <span class="placeholder col-12 h-loading"></span>
+      <span class="placeholder col-12 h-loading"></span>
+      <span class="placeholder col-12 h-loading"></span>
+      <span class="placeholder col-12 h-loading"></span>
+      <span class="placeholder col-12 h-loading"></span>
+      <span class="placeholder col-12 h-loading"></span>
+      <span class="placeholder col-12 h-loading"></span>
+      <span class="placeholder col-12 h-loading"></span>
+       <span class="placeholder col-12 h-loading"></span>
+      <span class="placeholder col-12 h-loading"></span>
+      <span class="placeholder col-12 h-loading"></span>
+      <span class="placeholder col-12 h-loading"></span>
+
+     
+   
+      
+    </p>
+  </div>
+
+  <div v-else class="table-scrollable">
     <table class="table table-striped">
       <thead>
         <tr>
           <th scope="col">id</th>
           <th scope="col">Name</th>
-          <th scope="col">Description</th>
           <th scope="col">Image</th>
-        
+          <th scope="col">Description</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
+        <tr v-for="beer in beers">
+          <th scope="row">{{beer.id}}</th>
+          <td>{{beer.name}}</td>
           <td>
             <img
-              src="https://github.com/mdo.png"
-              alt=""
-              width="32"
-              height="32"
-              class=" rounded-circle "
-            />
-          </td>
-          
-        </tr>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>
-            <img
-              src="https://github.com/mdo.png"
-              alt=""
-              width="32"
-              height="32"
-              class=" rounded-circle"
-            />
-          </td>
-         
-        </tr>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>
-            <img
-              src="https://github.com/mdo.png"
+              :src="beer.image"
               alt=""
               width="32"
               height="32"
               class="rounded-circle"
             />
           </td>
-          
+          <td>{{beer.description}}</td>
         </tr>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>
-            <img
-              src="https://github.com/mdo.png"
-              alt=""
-              width="32"
-              height="32"
-              class="rounded-circle"
-            />
-          </td>
-          
-        </tr>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>
-            <img
-              src="https://github.com/mdo.png"
-              alt=""
-              width="32"
-              height="32"
-              class="rounded-circle"
-            />
-          </td>
-         
-        </tr>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>
-            <img
-              src="https://github.com/mdo.png"
-              alt=""
-              width="32"
-              height="32"
-              class="rounded-circle"
-            />
-          </td>
-          
-        </tr>
-        
+       
+       
       </tbody>
     </table>
   </div>
-
-
 </template>
 
-<script>
-export default {
-  name: "categoryId",
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
 
-  async asyncData({ params }) {
-    const categoryId = params.id;
-    return { categoryId };
-  },
+const loading = ref(true);
+const categoriesData = ref({});
+const beers = ref([]);
+
+const fetchData = async () => {
+  try {
+    const route = useRoute();
+    const response = await fetch(
+      `http://localhost:3333/api/categories/${route.params.id}/beers`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    beers.value = data.beers;
+    console.log(`beers: `, beers);
+    categoriesData.value = data;
+
+    loading.value = false;
+  } catch (error) {
+    console.error("Error fetching categories:", error.message);
+  }
 };
+
+onMounted(() => {
+  
+  fetchData();
+});
 </script>
 
 <style>
@@ -156,7 +130,7 @@ export default {
 
 .table-scrollable {
   overflow-x: auto;
-  
+
   margin: auto;
 }
 </style>
